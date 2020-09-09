96d5c3f792ec445b49b0840ac83e9a21545849cc
Prasoon Gupta <1160576+0xdekster@users.noreply.github.com>
2020-09-09 18:21:35 +0530

    Update dekster.sh

--- dekster.sh
+++ dekster.sh
@@ -26,6 +26,7 @@ then
 cat /var/www/html/$1-$3/$1-subdomains.txt | httpx -status-code -title -json -o /var/www/html/$1-$3/$1-Httpx-output.json
 cat /var/www/html/$1-$3/$1-subdomains.txt | aquatone -out /var/www/html/$1-$3/$1-aqua-out
 sed -e 's|^[^/]*//||' -e 's|/.*$||' /var/www/html/$1-$3/$1-subdomains.txt | naabu -Pn | tee -a /var/www/html/$1-$3/ports-$1.txt
+nuclei -l /var/www/html/$1-$3/$1-subdomains.txt -t ./nuclei-templates/ -o /var/www/html/$1-$3/nuclei-$1.txt
 subjack -w /var/www/html/$1-$3/$1-subdomains.txt -t 100 -timeout 30 -o /var/www/html/$1-$3/subjack-$1.txt -ssl -c /root/tools/subjack/fingerprints.json
 for url in `cat /var/www/html/$1-$3/$1-subdomains.txt`; do gau $url | grep "\.js" | anew /var/www/html/$1-$3/js-$1.txt; done
 python3 /root/tools/dirsearch/dirsearch.py --url-list=/var/www/html/$1-$3/$1-subdomains.txt -e php,html,js,jar -b -t 50 -x 301,302,401,503,407,402,400,403,404,500,405,407,429,406,504,503,502,403,308 --plain-text-report=/var/www/html/$1-$3/$1-dirsearch.txt;
