f5540697f30c6e78d01737459991c5e3e305f370
Prasoon Gupta <1160576+0xdekster@users.noreply.github.com>
2020-09-13 17:32:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,12 +59,3 @@ echo "done"
 echo "Installing Dirsearch"
 git clone https://github.com/maurosoria/dirsearch.git
 echo "done"
-
-echo "Installing Nuclei"
-git clone https://github.com/projectdiscovery/nuclei.git
-cd nuclei/cmd/nuclei/ 
-go build 
-mv nuclei /usr/local/bin/
-cd ../../../
-nuclei -update-templates
-echo "done"
