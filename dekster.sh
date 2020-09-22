2aa420b885a8ea91dc641cc71b87d60f8569fc9c
Prasoon Gupta <1160576+0xdekster@users.noreply.github.com>
2020-09-22 16:03:47 +0530

    Update dekster.sh

--- dekster.sh
+++ dekster.sh
@@ -22,7 +22,7 @@ fi
 
 if [[ "$2" == "full_scan" ]]
 then
-python3 dirsearch/dirsearch.py -l /var/www/html/$1-$3/$1-subdomains.txt -e php,html,json,aspx -w ./dirsearch/db/dicc.txt --random-agents -b -t 100 -x 301,302,400,403,400,429,307,305 --plain-text-report=/var/www/html/$1-$3/$1-dirsearch.txt
+(python3 dirsearch/dirsearch.py -l /var/www/html/$1-$3/$1-subdomains.txt -e php,html,json,aspx -w ./dirsearch/db/dicc.txt --random-agents -b -t 100 -x 301,302,400,403,400,429,307,305 --plain-text-report=/var/www/html/$1-$3/$1-dirsearch.txt)
 cat /var/www/html/$1-$3/$1-subdomains.txt | httpx -status-code -title -json -o /var/www/html/$1-$3/$1-Httpx-output.json
 cat /var/www/html/$1-$3/$1-subdomains.txt | aquatone -out /var/www/html/$1-$3/$1-aqua-out
 sed -e 's|^[^/]*//||' -e 's|/.*$||' /var/www/html/$1-$3/$1-subdomains.txt | naabu | tee -a /var/www/html/$1-$3/ports-$1.txt
