d3a86cdbec3c331c385cdbc494b651fe71829c3e
Prasoon Gupta <1160576+0xdekster@users.noreply.github.com>
2020-10-09 09:43:25 +0530

    Update dekster.sh

--- dekster.sh
+++ dekster.sh
@@ -4,8 +4,8 @@ echo "$(tput setaf 2)Running Automation to gather data on" $1
 
 mkdir /var/www/html/$1-$3
 
-shuffledns -r ./resolvers.txt -d $1 -w ./subdomains.txt | anew /var/www/html/$1-$3/$1-subs.txt | ./findomain-linux -t $1 | anew /var/www/html/$1-$3/$1-subs.txt | subfinder -d $1 | anew /var/www/html/$1-$3/$1-subs.txt | shuffledns -r ./resolvers.txt | anew /var/www/html/$1-$3/$1-subs.txt
-sed -e 's|^[^/]*//||' -e 's|/.*$||' /var/www/html/$1-$3/$1-subs.txt | httprobe --prefer-https | anew /var/www/html/$1-$3/$1-subdomains.txt
+amass enum -passive -d $1 | tee -a /var/www/html/$1-$3/$1-subs.txt | subfinder -d $1 | anew /var/www/html/$1-$3/$1-subs.txt
+cat /var/www/html/$1-$3/$1-subs.txt | httprobe --prefer-https | anew /var/www/html/$1-$3/$1-subdomains.txt
 rm /var/www/html/$1-$3/$1-subs.txt
 
 if [[ "$2" == "port_scan" ]]
